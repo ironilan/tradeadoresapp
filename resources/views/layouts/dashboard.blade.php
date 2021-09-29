9f35896ccfbbe83d87d4f351ad9fa82dbdb80a18
Carlos <hi@carlosmacha.com>
2021-09-28 21:53:21 -0500

    cambios

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -49,6 +49,304 @@
         </div>
 
    </div>
+   
+
+    <div class="modal fade" id="modalcalculator" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content" style="background: #3a3361;">
+         
+          <div class="modal-body" >
+            
+
+                <div class="">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-xl-7 col-md-7 ">
+                                <div class="card settings_menu">
+                                    <div class="card-header">
+                                        <h4 class="card-title">Porcentajes Parciales</h4>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="form">
+                                            <div class="d-flex pb-4">                                                       
+                                                        <div class="flex-grow-1">
+                                                            <h5 class="mt-0 mb-1">Dif. Precios</h5>
+                                                                        
+                                                        </div>
+                                                        <div class="flex-grow-1">
+                                                            <h5 class="mt-0 mb-1">Puntos MT5</h5>
+                                                                        
+                                                        </div>
+                                                        <div class="edit-option">
+                                                            <h5 class="mt-0 mb-1">% Parcial</h5>
+                                                        </div>
+                                                        <div class="edit-option">
+                                                            
+                                                        </div>
+                                            </div>
+                                            
+                                            <ul class="linked_account">
+                                                
+                                               
+                                                
+                                              
+                                                <li class="pb-2">
+                                                    <div class="row">
+                                                        <div class="col-9">
+                                                            <div class="d-flex">
+                                                               
+                                                                <div class="flex-grow-1">
+                                                                    <h5 class="mt-0 mb-1">20.07</h5>
+                                                                    
+                                                                </div>
+                                                                <div class="flex-grow-1">
+                                                                    <h5 class="mt-0 mb-1">-</h5>
+                                                                    
+                                                                </div>
+                                                                
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-3">                                                            
+                                                                <input type="text" name="" class="form-control" placeholder="60">                                                            
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="row">
+                                                        <div class="col-9">
+                                                            <div class="d-flex">
+                                                               
+                                                                <div class="flex-grow-1">
+                                                                    <h5 class="mt-0 mb-1">-196.18</h5>
+                                                                    
+                                                                </div>
+                                                                <div class="flex-grow-1">
+                                                                    
+                                                                     <input style="text-align: center; width: 80%; margin: 0 40px;" type="text" name="" class="form-control " placeholder="400">
+                                                                    
+                                                                </div>
+                                                                
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-3">                                                            
+                                                                   <h5 class="mt-0 mb-1"> 576.49</h5>                                                        
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                                
+                                            </ul>
+
+                                          
+                                        </div>
+                                    </div>
+
+                                    
+                                </div>
+                            </div>
+                            <div class="col-xl-5 col-md-5">
+                                <div class="card ml-1" style="margin-left: 44px;">
+                                    <div class="card-header">
+                                        <h4 class="card-title">Plan Trading</h4>
+                                    </div>
+                                    
+                                    <div class="card-body">
+                                        <div class="form">
+                                            <div class="d-flex pb-4">                                                       
+                                                        <div class="flex-grow-1">
+                                                            <h5 class="mt-0 mb-1">Trailing Stop</h5>
+                                                                        
+                                                        </div>
+                                                        <div class="flex-grow-1">
+                                                            <h5 class="mt-0 mb-1">Precio</h5>
+                                                                        
+                                                        </div>
+                                                       
+                                            </div>
+                                            <ul class="linked_account">
+                                                <li>
+                                                    <div class="row">
+                                                        <div class="col-12">
+                                                            <div class="d-flex">
+                                                                <div class="flex-grow-1">
+                                                                    <h5 class="mt-0 mb-1">0%</h5>                                  
+                                                                </div>
+                                                                <div class="flex-grow-1 text-center">
+                                                                    <h5 class="mt-0 mb-1">30%</h5>                                   
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="row">
+                                                        <div class="col-12">
+                                                            <div class="d-flex">
+                                                                <div class="flex-grow-1">
+                                                                    <h5 class="mt-0 mb-1">20%</h5>                                  
+                                                                </div>
+                                                                <div class="flex-grow-1 text-center">
+                                                                    <h5 class="mt-0 mb-1">50%</h5>                                   
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </li>
+                                                 <li>
+                                                    <div class="row">
+                                                        <div class="col-12">
+                                                            <div class="d-flex">
+                                                                <div class="flex-grow-1">
+                                                                    <h5 class="mt-0 mb-1">50%</h5>                                  
+                                                                </div>
+                                                                <div class="flex-grow-1 text-center">
+                                                                    <h5 class="mt-0 mb-1">90%</h5>                                   
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </li>
+                                                 <li>
+                                                    <div class="row">
+                                                        <div class="col-12">
+                                                            <div class="d-flex">
+                                                                <div class="flex-grow-1">
+                                                                    <h5 class="mt-0 mb-1">90%</h5>                                  
+                                                                </div>
+                                                                <div class="flex-grow-1 text-center">
+                                                                    <h5 class="mt-0 mb-1">140%</h5>                                   
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="row">
+                                                        <div class="col-12">
+                                                            <div class="d-flex">
+                                                                <div class="flex-grow-1">
+                                                                    <h5 class="mt-0 mb-1">140%</h5>                                  
+                                                                </div>
+                                                                <div class="flex-grow-1 text-center">
+                                                                    <h5 class="mt-0 mb-1">200%</h5>                                   
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="row">
+                                                        <div class="col-12">
+                                                            <div class="d-flex">
+                                                                <div class="flex-grow-1">
+                                                                    <h5 class="mt-0 mb-1">200%</h5>                                  
+                                                                </div>
+                                                                <div class="flex-grow-1 text-center">
+                                                                    <h5 class="mt-0 mb-1">260%</h5>                                   
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </li>
+                                                
+                                            </ul>
+
+                                           
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Aceptar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="modaleditar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+      <div class="modal-dialog " role="document">
+        <div class="modal-content" style="background: #3a3361;">
+         
+          <div class="modal-body" >
+            
+
+                <div class="">
+                    <div class="container-fluid">
+                       
+                            
+                                <div class="card settings_menu">
+                                    <div class="card-header">
+                                        <h4 class="card-title">Editar Operación</h4>
+                                    </div>
+
+                                    <div class="card-body">
+                                        
+                                        <form action="verify-step-6.html" class="identity-upload p4">
+                                    <div class="row">
+                                        <div class="mb-3 col-xl-12">
+                                            <label class="me-sm-2">Precio Actual </label>
+                                            <input type="text" class="form-control" placeholder="25487">
+                                        </div>
+
+                                        <div class="mb-3 col-xl-12">
+                                            <label class="me-sm-2">Precio Salida </label>
+                                            <input type="text" class="form-control" placeholder="25487">
+                                        </div>
+
+                                        <div class="mb-3 col-xl-12">
+                                           <label class="me-sm-2">Estado</label>
+                                                    <div class="input-group mb-3">
+                                                        
+                                                        <select name='currency' class="form-control">
+                                                            <option value="">Selecciona</option>
+                                                            <option value="bitcoin">Abierto</option>
+                                                            <option value="litecoin">Cerrado</option>
+                                                        </select>
+                                                    </div>
+                                        </div>
+                                        
+                                       
+
+                                        <div class="text-center col-12">
+                                      
+                                            <button type="submit" class="btn btn-success mx-2 waves-effect">Actualizar</button>
+                                        </div>
+                                    </div>
+                                </form>
+
+                                    </div>
+
+                                    
+                                </div>
+                            
+                            
+                        
+                    </div>
+                </div>
+
+
+
+          </div>
+         
+        </div>
+      </div>
+    </div>
 
 
 
